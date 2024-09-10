fa5167d7e3f0a278015c8281b5c3dfe81ade47a1
Coty Hamilton <13724650+cotyhamilton@users.noreply.github.com>
2024-09-09 20:32:51 -0400

    Move data loading and processing to +page.ts

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,27 +1,14 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import { page } from "$app/stores";
-	import lyrics from "$lib/data/lyrics.json";
-	import type { Lyric } from "$lib/types/lyrics";
 	import * as Card from "$lib/components/ui/card";
 	import { Badge } from "$lib/components/ui/badge";
 	import { Input } from "$lib/components/ui/input";
 	import { backgroundImageMap } from "$lib/images";
 	import X from "lucide-svelte/icons/x";
 
-	let searchQuery = $page.url.searchParams.getAll("search").at(-1) ?? "";
-	let foundLyrics = $state<Lyric[]>([]);
-	let search = $state(searchQuery);
+	const { data } = $props();
 	let input = $state("");
-	let searchHistory = $state<string[]>($page.url.searchParams.getAll("search"));
-	let hasSearched = $state(searchQuery.length > 0);
-
-	function relativeLyric(from: number, to: -1 | 1) {
-		if (lyrics.data[from + to]?.song === lyrics.data[from].song) {
-			return lyrics.data[from + to].lyric;
-		}
-		return "...";
-	}
 
 	function appendSearchQueryString(word: string) {
 		const searchParams = new URLSearchParams($page.url.search);
@@ -34,47 +21,10 @@
 		searchParams.delete("search", word);
 		return searchParams.toString();
 	}
-
-	function count(item: "album" | "song") {
-		return foundLyrics
-			.map(({ [item]: el }) => el)
-			.filter((value, index, self) => self.indexOf(value) === index).length;
-	}
-
-	function findLyrics(word: string) {
-		const foundLyrics: Lyric[] = [];
-		if (word.length >= 3) {
-			lyrics.data.forEach(({ album, song, lyric, section }, index) => {
-				if (lyric.toLowerCase().includes(word.trim().toLowerCase())) {
-					foundLyrics.push({
-						album,
-						song,
-						lineNumber: index - lyrics.data.map(({ song }) => song).indexOf(song) + 1,
-						lyric,
-						prev: relativeLyric(index, -1),
-						next: relativeLyric(index, 1),
-						section
-					});
-				}
-			});
-		}
-		return foundLyrics;
-	}
-
-	foundLyrics = findLyrics(searchQuery);
-
-	$effect(() => {
-		search = ($page.url.searchParams.getAll("search").at(-1) ?? "").trim();
-		searchHistory = $page.url.searchParams.getAll("search");
-	});
-
-	$effect(() => {
-		foundLyrics = findLyrics(search);
-	});
 </script>
 
 <svelte:head>
-	<title>taylor swift lyrics</title>
+	<title>taylor swift data.lyrics</title>
 </svelte:head>
 
 <div class="mt-6 md:mx-12">
@@ -82,25 +32,23 @@
 		class="mb-4"
 		onsubmit={async (e) => {
 			e.preventDefault();
-			hasSearched = true;
 			if (input) {
-				searchHistory.push(input);
 				goto("?" + appendSearchQueryString(input));
 				input = "";
 			}
 		}}
 	>
-		<Input type="search" enterkeyhint="search" placeholder="Search lyrics" bind:value={input} />
+		<Input type="search" enterkeyhint="search" placeholder="search lyrics" bind:value={input} />
 	</form>
 
-	{#if hasSearched && search.length}
+	{#if data.search.length}
 		<p class="mb-4 text-sm text-muted-foreground">
-			{`Found ${foundLyrics.length} lyric${foundLyrics.length === 1 ? "" : "s"}, ${count("song")} song${count("song") === 1 ? "" : "s"}, and ${count("album")} album${count("album") === 1 ? "" : "s"}`}
+			{`found ${data.foundLyrics.length} lyric${data.foundLyrics.length === 1 ? "" : "s"}, ${data.songCount} song${data.songCount === 1 ? "" : "s"}, and ${data.albumCount} album${data.albumCount === 1 ? "" : "s"}`}
 		</p>
 	{/if}
 
 	<div class="my-4 flex gap-2 overflow-x-scroll">
-		{#each searchHistory as query}
+		{#each data.searchHistory as query}
 			<div class="flex items-center rounded-full bg-muted px-3 py-1 text-sm">
 				<span class="mr-2">
 					{query}
@@ -109,7 +57,6 @@
 					class="text-muted-foreground hover:text-foreground"
 					aria-label={`Remove ${query} from search history`}
 					onclick={() => {
-						searchHistory = searchHistory.filter((item) => item !== query);
 						goto("?" + popSearchQueryString(query));
 					}}
 				>
@@ -120,7 +67,7 @@
 	</div>
 
 	<div class="my-4 space-y-4">
-		{#each foundLyrics as { album, song, lineNumber, lyric, prev, next, section }}
+		{#each data.foundLyrics as { album, song, lineNumber, lyric, prev, next, section } (song + lineNumber)}
 			<Card.Root class={`${backgroundImageMap[album]} bg-cover bg-center text-primary-foreground`}>
 				<Card.Header>
 					<Card.Title>
@@ -159,7 +106,7 @@
 											class="hover:underline"
 											href="?${appendSearchQueryString(word.replace(",", "").toLowerCase())}"
 										>
-										${word.replace(new RegExp(search, "gi"), (match) => `<u>${match}</u>`)}
+										${word.replace(new RegExp(data.search, "gi"), (match) => `<u>${match}</u>`)}
 										</a>`
 								)
 								.join(" ")}
---
+++ src/routes/search/+page.ts
@@ -0,0 +1,50 @@
+import type { PageLoad } from "./$types";
+import lyrics from "$lib/data/lyrics.json";
+import type { Lyric } from "$lib/types/lyrics";
+
+function findLyrics(list: typeof lyrics.data, word: string): Lyric[] {
+	const foundLyrics: Lyric[] = [];
+	if (word.length >= 3) {
+		list.forEach(({ album, song, lyric, section }, index) => {
+			if (lyric.toLowerCase().includes(word.trim().toLowerCase())) {
+				foundLyrics.push({
+					album,
+					song,
+					lineNumber: index - list.map(({ song }) => song).indexOf(song) + 1,
+					lyric,
+					prev: relativeLyric(list, index, -1),
+					next: relativeLyric(list, index, 1),
+					section
+				});
+			}
+		});
+	}
+	return foundLyrics;
+}
+
+function relativeLyric(list: typeof lyrics.data, from: number, to: -1 | 1) {
+	if (list[from + to]?.song === list[from].song) {
+		return list[from + to].lyric;
+	}
+	return "...";
+}
+
+function count(list: Lyric[], item: "album" | "song") {
+	return list
+		.map(({ [item]: el }) => el)
+		.filter((value, index, self) => self.indexOf(value) === index).length;
+}
+
+export const load: PageLoad = ({ url }) => {
+	const searchHistory = url.searchParams.getAll("search");
+	const search = (url.searchParams.getAll("search").at(-1) ?? "").trim();
+	const foundLyrics = findLyrics(lyrics.data, search);
+
+	return {
+		search,
+		searchHistory,
+		foundLyrics,
+		albumCount: count(foundLyrics, "album"),
+		songCount: count(foundLyrics, "song")
+	};
+};
