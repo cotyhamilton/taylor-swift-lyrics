9ce121a15f51f909ef7b30dbcbd6863d21acad83
Coty Hamilton <13724650+cotyhamilton@users.noreply.github.com>
2025-02-03 22:27:41 -0500

    This time tho

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -14,11 +14,21 @@ export const handleError: HandleServerError = async ({ error, event }) => {
 	if (isHttpError(error) && error.status === 404) {
 		message = error.message;
 	} else {
-		const err =
+		const errorObj =
 			error instanceof Error
-				? { name: error.name, message: error.message, stack: error.stack }
+				? {
+						name: error.name,
+						message: error.message,
+						stack: error.stack,
+						...Object.fromEntries(
+							Object.entries(error).map(([key, value]) => [
+								key,
+								typeof value === "object" ? JSON.stringify(value) : value
+							])
+						)
+					}
 				: { message: String(error) };
-		event.locals.logger.error("Error", { error: err });
+		event.locals.logger.error("Error", { error: errorObj });
 	}
 
 	return {
